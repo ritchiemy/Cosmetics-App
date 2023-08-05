bf8d441d3575f225150fa88afdb8557dba6e4d20
ritchiemy <126090860+ritchiemy@users.noreply.github.com>
2023-08-04 21:46:25 -0400

    Update and rename config.toml to theme.py

---
+++ theme.py
@@ -0,0 +1,9 @@
+import streamlit as st
+def set_custom_theme(
+  layout="wide,
+  base="dark",
+  primaryColor="#ff0000",
+  secondaryBackgroundColor="#6b6b6b",
+  font="monospace")
+
+set_custom_theme()
