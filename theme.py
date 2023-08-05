3e471d7b93904eb2e6481e5695ce32bcc4209077
ritchiemy <126090860+ritchiemy@users.noreply.github.com>
2023-08-04 22:17:09 -0400

    Update theme.py

--- theme.py
+++ theme.py
@@ -1,9 +1,9 @@
 import streamlit as st
-def set_custom_theme(
-  layout="wide",
-  base="dark",
+def set_custom_theme():
   primaryColor="#ff0000",
-  secondaryBackgroundColor="#6b6b6b",
-  font="monospace",)
+  secondaryBackgroundColor="#6b6b6b"
+  base="dark"
+  
+  st.set_page_config(layout="wide")
 
 set_custom_theme()
